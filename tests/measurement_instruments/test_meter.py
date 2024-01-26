18db1e345e4d778ee2b43fa10c7a64fd7008a855
Peter Yefi <peteryefi@gmail.com>
2024-01-25 20:22:43 -0500

    Included test for meter and meter measure

---
+++ tests/measurement_instruments/test_meter.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+from datetime import datetime
+from measure_instruments import Meter
+from measure_instruments import MeterMeasure
+from enumerations import MeasurementUnit
+from enumerations import MeterType
+
+
+class TestMeter(TestCase):
+
+    def setUp(self) -> None:
+
+        self.meter = Meter("huz.cab.err", "Honeywell", 5, MeasurementUnit.KILOWATTS, MeterType.POWER)
+
+    def test_meter_without_manufacture(self):
+        try:
+            self.assertEqual(self.meter.meter_location, "huz.cab.err")
+            self.assertEqual(self.meter.meter_type, MeterType.POWER)
+            self.assertEqual(self.meter.measurement_unit.KILOWATTS.value, "KiloWatts")
+            self.meter.manufacturer = None
+        except ValueError as err:
+            self.assertEqual(err.__str__(), "Manufacturer must be a string")
+
+    def test_power_data_with_wrong_value(self):
+        try:
+            MeterMeasure("25")
+        except ValueError as err:
+            self.assertEqual(err.__str__(), "Value must be a float")
+
+    def test_power_meter_with_data(self):
+        self.meter.manufacturer = "Honeywell"
+        power_values = [2.5, 3.8, 9.7, 3.5]
+        for power in power_values:
+            self.meter.add_meter_measure(power)
+        self.assertEqual(len(self.meter.meter_measures), 4)
+        self.assertEqual(self.meter.meter_measures[0].value, 2.5)
+        self.assertIsNotNone(self.meter.meter_measures[0].UID)
+        self.assertEqual(self.meter.meter_measures[3].timestamp, datetime.now())
+
+
+
