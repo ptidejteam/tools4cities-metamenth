94b9157646a37d131f19e17fa409f0784d37ba88
peteryefi <peteryefi@gmail.com>
2024-11-01 14:37:35 -0400

    Updated version for production deployment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="metamenth",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(include=["metamenth", "metamenth.*"]),
     install_requires=requirements,
     entry_points={
